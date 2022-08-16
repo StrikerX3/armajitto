8ef7da7ccd606b7eac15033bcb265162f45e77ae
StrikerX3 <ivan.rober@gmail.com>
2022-08-16 11:58:12 -0300

    Use IR opcode constructors in emitter

--- src/ir/emitter.cpp
+++ src/ir/emitter.cpp
@@ -7,71 +7,42 @@ Variable Emitter::CreateVariable(const char *name) {
 }
 
 // TODO: avoid new; use allocator
-// TODO: feels like these things should be constructors in the op classes themselves
 // TODO: how will the optimizer work if we can only insert instructions at the end of a fragment?
 
 void Emitter::LoadGPR(VariableArg dst, GPRArg src) {
-    auto *op = new IRLoadGPROp();
-    op->dst = dst;
-    op->src = src;
-    ops.push_back(op);
+    ops.push_back(new IRLoadGPROp(dst, src));
 }
 
 void Emitter::StoreGPR(GPRArg dst, VarOrImmArg src) {
-    auto *op = new IRStoreGPROp();
-    op->dst = dst;
-    op->src = src;
-    ops.push_back(op);
+    ops.push_back(new IRStoreGPROp(dst, src));
 }
 
 void Emitter::LoadCPSR(VariableArg dst) {
-    auto *op = new IRLoadCPSROp();
-    op->dst = dst;
-    ops.push_back(op);
+    ops.push_back(new IRLoadCPSROp(dst));
 }
 
 void Emitter::StoreCPSR(VarOrImmArg src) {
-    auto *op = new IRStoreCPSROp();
-    op->src = src;
-    ops.push_back(op);
+    ops.push_back(new IRStoreCPSROp(src));
 }
 
 void Emitter::LoadSPSR(arm::Mode mode, VariableArg dst) {
-    auto *op = new IRLoadSPSROp();
-    op->mode = mode;
-    op->dst = dst;
-    ops.push_back(op);
+    ops.push_back(new IRLoadSPSROp(mode, dst));
 }
 
 void Emitter::StoreSPSR(arm::Mode mode, VarOrImmArg src) {
-    auto *op = new IRStoreSPSROp();
-    op->mode = mode;
-    op->src = src;
-    ops.push_back(op);
+    ops.push_back(new IRStoreSPSROp(mode, src));
 }
 
 void Emitter::MemRead(MemAccessMode mode, MemAccessSize size, VariableArg dst, VarOrImmArg address) {
-    auto *op = new IRMemReadOp();
-    op->mode = mode;
-    op->size = size;
-    op->dst = dst;
-    op->address = address;
-    ops.push_back(op);
+    ops.push_back(new IRMemReadOp(mode, size, dst, address));
 }
 
 void Emitter::MemWrite(MemAccessSize size, VarOrImmArg src, VarOrImmArg address) {
-    auto *op = new IRMemWriteOp();
-    op->size = size;
-    op->src = src;
-    op->address = address;
-    ops.push_back(op);
+    ops.push_back(new IRMemWriteOp(size, src, address));
 }
 
 void Emitter::CountLeadingZeros(VariableArg dst, VarOrImmArg value) {
-    auto *op = new IRCountLeadingZerosOp();
-    op->dst = dst;
-    op->value = value;
-    ops.push_back(op);
+    ops.push_back(new IRCountLeadingZerosOp(dst, value));
 }
 
 } // namespace armajitto::ir
