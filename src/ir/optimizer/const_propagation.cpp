b3315d096ac8ce56b3127de5f9498ae23b284dbf
StrikerX3 <ivan.rober@gmail.com>
2022-08-18 23:31:03 -0300

    Refine emitter API design

--- src/ir/optimizer/const_propagation.cpp
+++ src/ir/optimizer/const_propagation.cpp
@@ -9,35 +9,53 @@ void ConstPropagationOptimizerPass::Optimize(Emitter &emitter) {
     //
     // Possible API design:
     //   Manage cursor
+    //     size_t GetCodeSize()
     //     size_t GetCursorPos()
     //     void SetCursorPos(size_t index)
     //       index is clamped to size
     //       index == size means insert at the end
-    //     size_t GetCodeSize()
+    //       (assert that index <= size)
     //   Get writable pointer to instruction
-    //     IROp *GetOp(size_t index)   index >= size returns nullptr
+    //     IROp *GetOp(size_t index)
+    //       index >= size returns nullptr
+    //       (assert that index < size)
     //     IROp *GetCurrentOp()
     //       shorthand for emitter.GetOp(emitter.GetCursorPos())
     //   Overwrite one instruction
-    //     Emitter &Overwrite()   next emitted instruction will overwrite the current instruction
-    //       For complex instruction sequences, only the first instruction will overwrite; the rest of the sequence is
-    //       appended after that, which is the expected behavior
+    //     Emitter &Overwrite()
+    //       Next emitted instruction will overwrite the current instruction.
+    //       Returns the emitter itself for method chaining.
+    //       For complex instruction sequences, only the first instruction will overwrite. The rest of the sequence is
+    //       appended after that, which is the expected behavior.
     //   Erase instructions
-    //     void Erase(size_t pos, size_t count)
-    //       Erasing beyond the end of the vector is a no-op
+    //     void Erase(size_t pos, size_t count = 1)
+    //       Erasing beyond the end of the vector is a no-op.
     //       (i.e. pos and pos+count are clamped to size-1)
+    //       (though there should be an assert for correctness)
+    //     void EraseNext(size_t count = 1)
+    //       shorthand for Erase(GetCursorPos(), count)
     //
     // Usage examples:
-    //   // Begin inserting at specified position
-    //   emitter.SetCursorPos(3);
+    //   emitter.SetCursorPos(3);   // move insertion point to index 3 (i.e., between the 3rd and 4th elements)
     //   emitter.SetRegister(...);  // inserted at 3
     //   emitter.Add(...);          // inserted at 4
     //   emitter.Subtract(...);     // ... and so on
+    //
+    //   auto *op = emitter.GetCurrentOp();  // get op at the current cursor position
+    //   op-> ... ; // do something with it
     //
     //   emitter.Subtract(...);                 // insert
     //   emitter.Overwrite().Compare(...);      // overwrite
     //   emitter.BitClear(...);                 // insert
     //   emitter.Overwrite().UpdateFlags(...);  // overwrite first instruction, insert rest
+    // 
+    //   emitter.Erase
+    //
+    //   for (size_t i = 0; i < emitter.GetCodeSize(); i++) {
+    //       auto *op = emitter.GetOp(i);
+    //       op-> ... ; // manipulate op
+    //       // adding/removing instructions in this loop is not recommended
+    //   }
 }
 
 } // namespace armajitto::ir
