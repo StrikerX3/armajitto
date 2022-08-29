baf5e62ffb4b68697b9bf5c895efd166eeaa22c9
StrikerX3 <ivan.rober@gmail.com>
2022-08-29 20:22:58 -0300

    Handle TCM accesses in context

--- src/core/context.cpp
+++ src/core/context.cpp
@@ -22,12 +22,38 @@ Context::Context(CPUModel model, ISystem &system)
 }
 
 uint16_t Context::CodeReadHalf(uint32_t address) {
-    // TODO: handle TCM
+    auto &cp15 = m_armState.GetSystemControlCoprocessor();
+    if (cp15.IsPresent()) {
+        auto &tcm = cp15.GetTCM();
+        if (address < tcm.itcmReadSize) {
+            assert(std::popcount(tcm.itcmSize) == 1); // must be a power of two
+            const uint32_t addrMask = (tcm.itcmSize - 1) & ~1;
+            return *reinterpret_cast<uint16_t *>(&tcm.itcm[address & addrMask]);
+        }
+        if (address - tcm.dtcmBase < tcm.dtcmSize) {
+            assert(std::popcount(tcm.dtcmSize) == 1); // must be a power of two
+            const uint32_t addrMask = (tcm.dtcmSize - 1) & ~1;
+            return *reinterpret_cast<uint16_t *>(&tcm.dtcm[address & addrMask]);
+        }
+    }
     return m_system.MemReadHalf(address);
 }
 
 uint32_t Context::CodeReadWord(uint32_t address) {
-    // TODO: handle TCM
+    auto &cp15 = m_armState.GetSystemControlCoprocessor();
+    if (cp15.IsPresent()) {
+        auto &tcm = cp15.GetTCM();
+        if (address < tcm.itcmReadSize) {
+            assert(std::popcount(tcm.itcmSize) == 1); // must be a power of two
+            const uint32_t addrMask = (tcm.itcmSize - 1) & ~3;
+            return *reinterpret_cast<uint32_t *>(&tcm.itcm[address & addrMask]);
+        }
+        if (address - tcm.dtcmBase < tcm.dtcmSize) {
+            assert(std::popcount(tcm.dtcmSize) == 1); // must be a power of two
+            const uint32_t addrMask = (tcm.dtcmSize - 1) & ~3;
+            return *reinterpret_cast<uint32_t *>(&tcm.dtcm[address & addrMask]);
+        }
+    }
     return m_system.MemReadWord(address);
 }
 
