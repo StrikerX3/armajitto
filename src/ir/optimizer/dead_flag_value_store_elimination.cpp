1cefcd674be9562753d01ad892937787d91d5cca
StrikerX3 <ivan.rober@gmail.com>
2022-09-01 23:38:35 -0300

    Dead flag value store elimination can't eliminate flags from consumed values

--- src/ir/optimizer/dead_flag_value_store_elimination.cpp
+++ src/ir/optimizer/dead_flag_value_store_elimination.cpp
@@ -15,14 +15,66 @@ DeadFlagValueStoreEliminationOptimizerPass::DeadFlagValueStoreEliminationOptimiz
     m_flagWritesPerVar.resize(varCount);
 }
 
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRSetRegisterOp *op) {
+    ConsumeFlags(op->src);
+}
+
 void DeadFlagValueStoreEliminationOptimizerPass::Process(IRGetCPSROp *op) {
     InitFlagWrites(op->dst);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRSetCPSROp *op) {
+    ConsumeFlags(op->src);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRSetSPSROp *op) {
+    ConsumeFlags(op->src);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRMemReadOp *op) {
+    ConsumeFlags(op->address);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRMemWriteOp *op) {
+    ConsumeFlags(op->src);
+    ConsumeFlags(op->address);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRPreloadOp *op) {
+    ConsumeFlags(op->address);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRLogicalShiftLeftOp *op) {
+    ConsumeFlags(op->value);
+    ConsumeFlags(op->amount);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRLogicalShiftRightOp *op) {
+    ConsumeFlags(op->value);
+    ConsumeFlags(op->amount);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRArithmeticShiftRightOp *op) {
+    ConsumeFlags(op->value);
+    ConsumeFlags(op->amount);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRRotateRightOp *op) {
+    ConsumeFlags(op->value);
+    ConsumeFlags(op->amount);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRRotateRightExtendedOp *op) {
+    ConsumeFlags(op->value);
 }
 
 void DeadFlagValueStoreEliminationOptimizerPass::Process(IRBitwiseAndOp *op) {
     if (auto split = SplitImmVarPair(op->lhs, op->rhs)) {
         auto [imm, var] = *split;
         RecordFlagWrites(op->dst, var, static_cast<arm::Flags>(imm), op);
+    } else {
+        ConsumeFlags(op->lhs);
+        ConsumeFlags(op->rhs);
     }
 }
 
@@ -30,20 +82,106 @@ void DeadFlagValueStoreEliminationOptimizerPass::Process(IRBitwiseOrOp *op) {
     if (auto split = SplitImmVarPair(op->lhs, op->rhs)) {
         auto [imm, var] = *split;
         RecordFlagWrites(op->dst, var, static_cast<arm::Flags>(imm), op);
+    } else {
+        ConsumeFlags(op->lhs);
+        ConsumeFlags(op->rhs);
     }
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRBitwiseXorOp *op) {
+    ConsumeFlags(op->lhs);
+    ConsumeFlags(op->rhs);
 }
 
 void DeadFlagValueStoreEliminationOptimizerPass::Process(IRBitClearOp *op) {
     if (auto split = SplitImmVarPair(op->lhs, op->rhs)) {
         auto [imm, var] = *split;
         RecordFlagWrites(op->dst, var, static_cast<arm::Flags>(imm), op);
+    } else {
+        ConsumeFlags(op->lhs);
+        ConsumeFlags(op->rhs);
     }
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRCountLeadingZerosOp *op) {
+    ConsumeFlags(op->value);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRAddOp *op) {
+    ConsumeFlags(op->lhs);
+    ConsumeFlags(op->rhs);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRAddCarryOp *op) {
+    ConsumeFlags(op->lhs);
+    ConsumeFlags(op->rhs);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRSubtractOp *op) {
+    ConsumeFlags(op->lhs);
+    ConsumeFlags(op->rhs);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRSubtractCarryOp *op) {
+    ConsumeFlags(op->lhs);
+    ConsumeFlags(op->rhs);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRMoveOp *op) {
+    ConsumeFlags(op->value);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRMoveNegatedOp *op) {
+    ConsumeFlags(op->value);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRSaturatingAddOp *op) {
+    ConsumeFlags(op->lhs);
+    ConsumeFlags(op->rhs);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRSaturatingSubtractOp *op) {
+    ConsumeFlags(op->lhs);
+    ConsumeFlags(op->rhs);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRMultiplyOp *op) {
+    ConsumeFlags(op->lhs);
+    ConsumeFlags(op->rhs);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRMultiplyLongOp *op) {
+    ConsumeFlags(op->lhs);
+    ConsumeFlags(op->rhs);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRAddLongOp *op) {
+    ConsumeFlags(op->lhsLo);
+    ConsumeFlags(op->lhsHi);
+    ConsumeFlags(op->rhsLo);
+    ConsumeFlags(op->rhsHi);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRStoreFlagsOp *op) {
+    ConsumeFlags(op->values);
 }
 
 void DeadFlagValueStoreEliminationOptimizerPass::Process(IRLoadFlagsOp *op) {
     if (!op->srcCPSR.immediate) {
         RecordFlagWrites(op->dstCPSR, op->srcCPSR.var, op->flags, op);
     }
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRBranchOp *op) {
+    ConsumeFlags(op->address);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRBranchExchangeOp *op) {
+    ConsumeFlags(op->address);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::Process(IRStoreCopRegisterOp *op) {
+    ConsumeFlags(op->srcValue);
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -98,6 +236,26 @@ void DeadFlagValueStoreEliminationOptimizerPass::RecordFlagWrites(VariableArg ds
     updateWrite(arm::Flags::Z, srcEntry.writerOpZ, dstEntry.writerOpZ);
     updateWrite(arm::Flags::C, srcEntry.writerOpC, dstEntry.writerOpC);
     updateWrite(arm::Flags::V, srcEntry.writerOpV, dstEntry.writerOpV);
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::ConsumeFlags(VariableArg &arg) {
+    if (!arg.var.IsPresent()) {
+        return;
+    }
+    const auto varIndex = arg.var.Index();
+    if (varIndex >= m_flagWritesPerVar.size()) {
+        return;
+    }
+    m_flagWritesPerVar[varIndex].writerOpN = nullptr;
+    m_flagWritesPerVar[varIndex].writerOpZ = nullptr;
+    m_flagWritesPerVar[varIndex].writerOpC = nullptr;
+    m_flagWritesPerVar[varIndex].writerOpV = nullptr;
+}
+
+void DeadFlagValueStoreEliminationOptimizerPass::ConsumeFlags(VarOrImmArg &arg) {
+    if (!arg.immediate) {
+        ConsumeFlags(arg.var);
+    }
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
