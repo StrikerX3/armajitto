b08feec38185a2144c2e033437209ecb82506375
StrikerX3 <ivan.rober@gmail.com>
2022-09-14 22:08:51 -0300

    More fuzzing

--- fuzzer/main.cpp
+++ fuzzer/main.cpp
@@ -1,6 +1,7 @@
 #include <armajitto/armajitto.hpp>
 
 #include <algorithm>
+#include <random>
 
 #include "interp.hpp"
 #include "system.hpp"
@@ -68,7 +69,7 @@ int main(int argc, char *argv[]) {
                flag(psr.q, 'Q'), flag(psr.i, 'I'), flag(psr.f, 'F'));
     };
 
-    auto printStates = [&] {
+    auto printStates = [&](bool printMemory) {
         printf("Interpreter state\n");
         for (int j = 0; j < 4; j++) {
             for (int i = 0; i < 4; i++) {
@@ -83,21 +84,23 @@ int main(int argc, char *argv[]) {
             printf("\n");
         }
         printPSR({.u32 = interp->GetCPSR()}, "CPSR");
-        printf("Memory:\n");
-        for (int i = 0; i < 16; i++) {
-            printf("%02X |", i * 16);
-            for (int j = 0; j < 16; j++) {
-                printf(" %02X", interpSys.mem[j + i * 16]);
+        if (printMemory) {
+            printf("Memory:\n");
+            for (int i = 0; i < 16; i++) {
+                printf("  %02X |", i * 16);
+                for (int j = 0; j < 16; j++) {
+                    printf(" %02X", interpSys.mem[j + i * 16]);
+                }
+                printf("\n");
             }
-            printf("\n");
-        }
-        printf("Code memory:\n");
-        for (int i = 0; i < 16; i++) {
-            printf("%02X |", i * 16);
-            for (int j = 0; j < 16; j++) {
-                printf(" %02X", interpSys.codemem[j + i * 16]);
+            printf("Code memory:\n");
+            for (int i = 0; i < 16; i++) {
+                printf("  %02X |", i * 16);
+                for (int j = 0; j < 16; j++) {
+                    printf(" %02X", interpSys.codemem[j + i * 16]);
+                }
+                printf("\n");
             }
-            printf("\n");
         }
 
         printf("------------------------------------------------------------------------\n");
@@ -116,25 +119,28 @@ int main(int argc, char *argv[]) {
             printf("\n");
         }
         printPSR(jitState.CPSR(), "CPSR");
-        printf("Memory:\n");
-        for (int i = 0; i < 16; i++) {
-            printf("%02X |", i * 16);
-            for (int j = 0; j < 16; j++) {
-                printf(" %02X", jitSys.mem[j + i * 16]);
+        if (printMemory) {
+            printf("Memory:\n");
+            for (int i = 0; i < 16; i++) {
+                printf("  %02X |", i * 16);
+                for (int j = 0; j < 16; j++) {
+                    printf(" %02X", jitSys.mem[j + i * 16]);
+                }
+                printf("\n");
             }
-            printf("\n");
-        }
-        printf("Code memory:\n");
-        for (int i = 0; i < 16; i++) {
-            printf("%02X |", i * 16);
-            for (int j = 0; j < 16; j++) {
-                printf(" %02X", jitSys.codemem[j + i * 16]);
+            printf("Code memory:\n");
+            for (int i = 0; i < 16; i++) {
+                printf("  %02X |", i * 16);
+                for (int j = 0; j < 16; j++) {
+                    printf(" %02X", jitSys.codemem[j + i * 16]);
+                }
+                printf("\n");
             }
-            printf("\n");
         }
+        printf("\n");
     };
 
-    auto compareStates = [&](auto errorAction) {
+    auto compareStates = [&](bool printMismatch, auto errorAction) {
         bool anyMismatch = false;
 
         auto setMismatch = [&] {
@@ -180,12 +186,12 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        /*if (anyMismatch) {
+        if (anyMismatch && printMismatch) {
             printf("\n");
             printf("========================================================\n");
-            printStates();
+            printStates(true);
             printf("========================================================\n");
-        }*/
+        }
     };
 
     auto init = [&](arm::Mode mode, uint32_t address, bool thumb) {
@@ -249,7 +255,7 @@ int main(int argc, char *argv[]) {
         printf("===============================\n");
         printf("Testing mode %d\n\n", mode);
         init(mode, 0x10000, true);
-        printStates();
+        printStates(false);
         printf("\n");
 
         for (uint32_t instr = 0; instr <= 0xFFFF; instr++) {
@@ -272,7 +278,8 @@ int main(int argc, char *argv[]) {
             jit.Run(1);
 
             // Compare states and print any discrepancies
-            compareStates([&] { printf("[!] Discrepancies found on mode %d, instruction %04X\n", mode, instr); });
+            compareStates(false, [&] { printf("[!] Discrepancies found on mode %d, instruction %04X\n", mode, instr);
+    });
         }
     }*/
 
@@ -281,18 +288,19 @@ int main(int argc, char *argv[]) {
     // - System uses all base registers
     // - IRQ has its own R13 and R14
     // - FIQ has its own R8 through R14
-    for (auto mode : {arm::Mode::System, arm::Mode::IRQ, arm::Mode::FIQ}) {
+    /*for (auto mode : {arm::Mode::System, arm::Mode::IRQ, arm::Mode::FIQ}) {
         printf("===============================\n");
         printf("Testing mode %d\n\n", mode);
         init(mode, 0x10000, false);
-        printStates();
+        printStates(false);
         printf("\n");
         fprintf(stderr, "Testing mode %d\n", mode);
 
 #ifdef _DEBUG
         const uint32_t testInstr = 0xE1CF0080;
         const uint32_t start = testInstr - 0xE0000000;
-        const uint32_t end = start /*+ 0x1000000*/ + 1;
+        // const uint32_t end = start + 1;
+        const uint32_t end = start + 0x1000000 + 1;
 #else
         const uint32_t start = offset * 0x1000000;
         const uint32_t end = start + limit * 0x1000000;
@@ -322,8 +330,164 @@ int main(int argc, char *argv[]) {
             jit.Run(1);
 
             // Compare states and print any discrepancies
-            compareStates([&] { printf("[!] Discrepancies found on mode %d, instruction %08X\n", mode, instr); });
+            compareStates(false,
+                          [&] { printf("[!] Discrepancies found on mode %d, instruction %08X\n", mode, instr); });
         }
+    }*/
+
+    // Test random sequences of ARM instructions in selected modes.
+    // The entire code memory is filled with random bytes and both the interpreter and JIT run for 64 cycles.
+    // These modes differ in the banked registers used:
+    // - System uses all base registers
+    // - IRQ has its own R13 and R14
+    // - FIQ has its own R8 through R14
+    /*std::default_random_engine generator;
+    std::uniform_int_distribution<uint32_t> distribution{};
+
+    const uint64_t numIters = 100;
+    static constexpr auto blockSize = jitSys.codemem.size() / sizeof(uint32_t);
+
+    // Precompute random data to be reused across different modes
+    std::vector<uint32_t> randomData{};
+    randomData.resize(numIters * blockSize);
+    std::generate(randomData.begin(), randomData.end(), [&] { return distribution(generator); });
+
+    // Configure the JIT
+    jit.GetOptions().translator.maxBlockSize = blockSize;
+    jit.GetOptions().compiler.enableBlockLinking = true;
+
+    for (auto mode : {arm::Mode::System, arm::Mode::IRQ, arm::Mode::FIQ}) {
+        printf("===============================\n");
+        printf("Testing mode %d\n\n", mode);
+        init(mode, 0x10000, true);
+        // printStates(false);
+        printf("\n");
+
+        for (uint64_t iteration = 0; iteration < numIters; iteration++) {
+            // Reset interpreter and JIT
+            interp->Reset();
+            jit.Reset();
+
+            // Reset system memory
+            interpSys.Reset();
+            jitSys.Reset();
+
+            // Fill code memory with random data
+            std::copy_n(&randomData[iteration * blockSize], blockSize,
+                        reinterpret_cast<uint32_t *>(&jitSys.codemem[0]));
+            interpSys.codemem = jitSys.codemem;
+
+            init(mode, 0x10000, false);
+
+            // Run both the interpreter and the JIT for 64 cycles
+            auto cyclesExecuted = jit.Run(64);
+            interp->Run(cyclesExecuted);
+
+            // Compare states and print any discrepancies
+            compareStates(true, [&] { printf("[!] Discrepancies found on mode %d\n", mode); });
+        }
+    }*/
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // Test a fixed sequence of ARM instructions in selected modes.
+    // These modes differ in the banked registers used:
+    // - System uses all base registers
+    // - IRQ has its own R13 and R14
+    // - FIQ has its own R8 through R14
+
+    std::vector<uint8_t> code;
+    uint32_t numInstrs = 0;
+    auto writeInstr = [&](uint32_t instr) {
+        code.push_back(instr >> 0);
+        code.push_back(instr >> 8);
+        code.push_back(instr >> 16);
+        code.push_back(instr >> 24);
+        ++numInstrs;
+    };
+
+    // writeInstr(0xE3B004DE); // movs r0, #0xDE000000
+    // writeInstr(0x039008AD); // orrseq r0, #0x00AD0000
+    // writeInstr(0x03900CBE); // orrseq r0, #0x0000BE00
+    // writeInstr(0x039000EF); // orrseq r0, #0x000000EF
+    // writeInstr(0xE2801008); // add r1, r0, #0x8
+    // writeInstr(0xE01120D1); // ldrsb r2, [r1], -r1
+    // writeInstr(0xE19230F2); // ldrsh r3, [r2, r2]
+    // writeInstr(0xE3A04000); // mov r4, #0x0
+    // writeInstr(0xE18030B4); // strh r3, [r0, r4]
+    // writeInstr(0xE114F253); // tst r4, r3, asr r2  (with hidden PC)
+    // writeInstr(0x297040FC); // ldmdbcs r0!, {r2-r7, lr}^
+    // writeInstr(0xEF060000); // swi #0x60000
+
+    // writeInstr(0xE92D500F); // stmdb sp!, {r0-r3, r12, lr}
+    // writeInstr(0xEE190F11); // mrc p15, 0, r0, c9, c1, 0
+    // writeInstr(0xE1A00620); // mov r0, r0, lsr #0xC
+    // writeInstr(0xE1A00600); // mov r0, r0, lsl #0xC
+    // writeInstr(0xE2800C40); // add r0, r0, #0x4000
+    // writeInstr(0xE28FE000); // add lr, pc, #0x0
+    // writeInstr(0xE510F004); // ldr pc, [r0, #-0x4]
+
+    // writeInstr(0xE8BD4004); // ldmia sp!, {r2, lr}
+    // writeInstr(0xE3A0C0D3); // mov r12, #0xD3
+    // writeInstr(0xE12FF00C); // msr cpsr_fsxc, r12
+    // writeInstr(0xE8BD0800); // ldmia sp!, {r11}
+    // writeInstr(0xE16FF00B); // msr spsr_fsxc, r11
+    // writeInstr(0xE8BD5800); // ldmia sp!, {r11, r12, lr}
+    // writeInstr(0xE1B0F00E); // movs pc, lr
+
+    numInstrs = 64;
+    code.insert(code.end(),
+                {
+                    0xA7, 0x52, 0x0A, 0xD3, 0x0F, 0x8B, 0x37, 0x88, 0x12, 0xD4, 0x8F, 0xFB, 0x27, 0x70, 0x41, 0xE6,
+                    0xA7, 0x95, 0xF1, 0xBA, 0xCD, 0xD5, 0xDC, 0xC5, 0xDA, 0x52, 0x08, 0x58, 0x6E, 0x98, 0x29, 0x34,
+                    0x46, 0x91, 0x85, 0x95, 0x4A, 0x45, 0x38, 0xE8, 0x04, 0xC0, 0x96, 0x1B, 0x4D, 0xB7, 0xF3, 0x5F,
+                    0x9C, 0xCF, 0x03, 0xE8, 0x75, 0x5A, 0x1C, 0xD9, 0x96, 0xFC, 0x30, 0xE1, 0xD0, 0x8F, 0xA2, 0x53,
+                    0x94, 0xC1, 0x58, 0xD1, 0x85, 0x03, 0xA5, 0x53, 0xF5, 0x11, 0xBF, 0x42, 0xF5, 0x06, 0xC2, 0xA8,
+                    0x23, 0xBB, 0x27, 0x98, 0xD9, 0x3A, 0x36, 0x96, 0xA9, 0x62, 0xC3, 0x05, 0xC4, 0x29, 0x8A, 0x0C,
+                    0x78, 0xCB, 0xDD, 0x6C, 0x48, 0x78, 0x1C, 0xC5, 0x5A, 0x58, 0x0E, 0x50, 0x64, 0x78, 0xE3, 0x7D,
+                    0x6E, 0x10, 0x57, 0x29, 0xAB, 0xA0, 0xF9, 0x7C, 0xE9, 0x9E, 0xC3, 0x2D, 0x2B, 0x3D, 0xDB, 0x6D,
+                    0x98, 0x3C, 0x42, 0x6C, 0x24, 0xE0, 0x0C, 0x65, 0xEB, 0x69, 0x1F, 0x18, 0x24, 0xAD, 0xC5, 0xC6,
+                    0xC1, 0xD8, 0x38, 0x99, 0xE3, 0x80, 0x89, 0xF9, 0xE8, 0x7D, 0x8E, 0x78, 0xBC, 0x08, 0x9E, 0xBF,
+                    0xE9, 0xBB, 0x29, 0xB2, 0x2B, 0xB9, 0x6E, 0xA2, 0xAC, 0xD9, 0x2B, 0xB3, 0xCA, 0x67, 0x7A, 0x12,
+                    0x62, 0xC0, 0x76, 0xA3, 0xD6, 0x18, 0x24, 0xA2, 0xD0, 0x42, 0x9A, 0x08, 0xB3, 0x57, 0x67, 0x87,
+                    0xDA, 0x46, 0x9D, 0x11, 0xA8, 0xA4, 0xD1, 0x4B, 0xD4, 0x49, 0xD1, 0x51, 0xEE, 0xAF, 0x48, 0x53,
+                    0xAF, 0xB8, 0xE6, 0x87, 0x4A, 0x13, 0x91, 0xA1, 0xF8, 0xC0, 0x89, 0xA7, 0x49, 0xE8, 0x97, 0xFE,
+                    0x5C, 0xBB, 0x59, 0x68, 0xDB, 0xB5, 0x1B, 0x84, 0x06, 0x4A, 0xEA, 0xD1, 0xCA, 0xCF, 0x75, 0xB9,
+                    0x3F, 0x5F, 0xE6, 0xB7, 0x98, 0xFC, 0xDD, 0x3E, 0x15, 0x67, 0xF9, 0xF7, 0x20, 0x2D, 0x14, 0x55,
+                });
+
+    // Configure the JIT
+    jit.GetOptions().translator.maxBlockSize = numInstrs;
+    jit.GetOptions().compiler.enableBlockLinking = true;
+
+    for (auto mode : {arm::Mode::System, arm::Mode::IRQ, arm::Mode::FIQ}) {
+        printf("===============================\n");
+        printf("Testing mode %d\n\n", mode);
+        printf("\n");
+
+        // Reset interpreter and JIT
+        interp->Reset();
+        jit.Reset();
+
+        // Reset system memory
+        interpSys.Reset();
+        jitSys.Reset();
+
+        // Fill code memory with random data
+        std::copy(code.begin(), code.end(), jitSys.codemem.begin());
+        interpSys.codemem = jitSys.codemem;
+
+        init(mode, 0x10000, false);
+
+        // Run both the interpreter and the JIT
+        auto cyclesExecuted = jit.Run(numInstrs);
+        interp->Run(cyclesExecuted);
+
+        printStates(true);
+        printf("%llu cycles executed\n\n", cyclesExecuted);
+
+        // Compare states and print any discrepancies
+        compareStates(false, [&] { printf("[!] Discrepancies found on mode %d\n", mode); });
     }
 
     return 0;
