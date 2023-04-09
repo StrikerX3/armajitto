22c741221249dcb1620099725f3905ec94cd82dd
StrikerX3 <ivan.rober@gmail.com>
2023-04-08 22:55:56 -0300

    Dereference MemoryMap pImpl pointer once per function call

--- src/core/memory_map.cpp
+++ src/core/memory_map.cpp
@@ -11,28 +11,30 @@ MemoryMap::~MemoryMap() = default;
 
 void MemoryMap::Map(MemoryArea areas, uint8_t layer, uint32_t baseAddress, uint32_t size, MemoryAttributes attrs,
                     uint8_t *ptr, uint64_t mirrorSize) {
+    auto &impl = *m_impl;
     auto bmAreas = BitmaskEnum(areas);
     if (bmAreas.AllOf(MemoryArea::CodeRead)) {
-        m_impl->codeRead.Map(layer, baseAddress, size, attrs, ptr, mirrorSize);
+        impl.codeRead.Map(layer, baseAddress, size, attrs, ptr, mirrorSize);
     }
     if (bmAreas.AllOf(MemoryArea::DataRead)) {
-        m_impl->dataRead.Map(layer, baseAddress, size, attrs, ptr, mirrorSize);
+        impl.dataRead.Map(layer, baseAddress, size, attrs, ptr, mirrorSize);
     }
     if (bmAreas.AllOf(MemoryArea::DataWrite)) {
-        m_impl->dataWrite.Map(layer, baseAddress, size, attrs, ptr, mirrorSize);
+        impl.dataWrite.Map(layer, baseAddress, size, attrs, ptr, mirrorSize);
     }
 }
 
 void MemoryMap::Unmap(MemoryArea areas, uint8_t layer, uint32_t baseAddress, uint64_t size) {
+    auto &impl = *m_impl;
     auto bmAreas = BitmaskEnum(areas);
     if (bmAreas.AllOf(MemoryArea::CodeRead)) {
-        m_impl->codeRead.Unmap(layer, baseAddress, size);
+        impl.codeRead.Unmap(layer, baseAddress, size);
     }
     if (bmAreas.AllOf(MemoryArea::DataRead)) {
-        m_impl->dataRead.Unmap(layer, baseAddress, size);
+        impl.dataRead.Unmap(layer, baseAddress, size);
     }
     if (bmAreas.AllOf(MemoryArea::DataWrite)) {
-        m_impl->dataWrite.Unmap(layer, baseAddress, size);
+        impl.dataWrite.Unmap(layer, baseAddress, size);
     }
 }
 
